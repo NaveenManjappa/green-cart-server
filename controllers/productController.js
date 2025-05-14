bb59e24919ac9e30be976b9d59e5b05e1bd15510
Naveen Manjappa <NManjappa@manh.com>
2025-05-14 07:30:41 +0100

    Implement product controller with cloudinary setup

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,20 +1,59 @@
+import { v2 as cloudinary} from "cloudinary";
+import Product from "../models/Product.js";
+import { request } from "express";
+
 // Add product: api/product/add
 export const addProduct = async (req,res) => {
+  try {
+    let productData =JSON.parse(req.body.productData);
+    const images = req.files;
+    let imagesUrl = await Promise.all(
+      images.map(async (image)=> {
+        let result = await cloudinary.uploader.upload(image.path,{resource_type:'image'});
+        return result.secure_url;
+      })
+    )
+    await Product.create({...productData,image:imagesUrl});
 
+    return res.json({success:true,message:'Product added'});
+  } catch (error) {
+    console.error(error);
+    return res.json({success:false,message:error.message});
+  }
 }
 
 
 //Get product: api/product/list
 export const productList = async (req,res) => {
-
+try {
+  const products = await Product.find({});
+  return res.json({success:true,products})
+} catch (error) {
+   console.error(error);
+   return res.json({success:false,message:error.message});
+}
 }
 
 //Get single product: api/product/id
 export const productById = async (req,res) => {
-
+try {
+  const { id } = req.body;
+  const product = await Product.findById(id);
+  return res.json({success:true,product});
+} catch (error) {
+  console.error(error);
+    return res.json({success:false,message:error.message});
+}
 }
 
 //Change product inStock: api/product/stock
 export const changeStock = async(req,res) => {
-  
+  try {
+    const {id,inStock} = request.body;
+    await Product.findByIdAndUpdate(id,{inStock});
+    return res.json({success:true,message:'Stock updated'});
+  } catch (error) {
+    console.error(error);
+    return res.json({success:false,message:error.message});
+  }
 }
--- server.js
+++ server.js
@@ -5,12 +5,16 @@ import connectDB from './configs/db.js';
 import 'dotenv/config';
 import userRouter from './routes/userRoute.js';
 import sellerRouter from './routes/sellerRoute.js';
+import connectCloudinary from './configs/cloudinary.js';
+import productRouter from './routes/productRoute.js';
 
 const app = express();
 
 const port = process.env.PORT || 4000;
 
 await connectDB();
+
+await connectCloudinary();
 
 const allowedOrigins = ['http://localhost:5173'];
 
@@ -25,6 +29,7 @@ return res.send("API is working");
 
 app.use('/api/user',userRouter);
 app.use('/api/seller',sellerRouter);
+app.use('/api/product',productRouter);
 
 app.listen(port,()=>{
   console.log(`Server is running on http://localhost:${port}`);
